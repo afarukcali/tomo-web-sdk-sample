e74a8f9968948419dbcff39d59938671d2dd36e8
afarukcali <afarukcali@gmail.com>
2025-04-02 20:38:49 +0300

    Refactor ConnectWrapper component for improved readability and import style.css directly in ConnectWrapper.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import ConnectWrapper from "./ConnectWrapper";
-import "@tomo-inc/tomo-web-sdk/style.css";
 import TomoConnect from "./TomoConnect";
 
 export default function Home() {
-    return (
-        <ConnectWrapper>
-            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <TomoConnect />
-            </div>
-        </ConnectWrapper>
-    );
+  return (
+    <ConnectWrapper>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <TomoConnect />
+      </div>
+    </ConnectWrapper>
+  );
 }
